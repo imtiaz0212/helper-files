0f99f21e6e344f1b0bf8a39398aa1884a9fad1da
Imtiaz Ahammed <88791780+imtiaz0212@users.noreply.github.com>
2024-02-27 21:16:33 +0600

    htaccess redirect www.domain.com

--- laravel-helper.php
+++ laravel-helper.php
@@ -1,2 +1,8 @@
 // laravel ignore php platform 
 composer --ignore-platform-req=php update
+
+// .htaccess
+RewriteCond %{HTTP_HOST} ^www\domain\.com
+RewriteRule ^(.*)$ https://domain.com$1 [R=301,L]
+
+
