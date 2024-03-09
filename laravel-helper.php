d64050b21e563cefac6cb1fd0dc6e4be16a44d6f
Imtiaz Ahammed <88791780+imtiaz0212@users.noreply.github.com>
2024-03-09 11:09:04 +0600

    Update laravel-helper.php

--- laravel-helper.php
+++ laravel-helper.php
@@ -1,6 +1,8 @@
 // laravel ignore php platform 
 composer --ignore-platform-req=php update
 
+composer install --optimize-autoloader --no-dev
+
 // .htaccess
 RewriteCond %{HTTP_HOST} ^www\domain\.com
 RewriteRule ^(.*)$ https://domain.com$1 [R=301,L]
