342185d68b0f68336fc6addec83b6e0cececb672
Imtiaz Ahammed <88791780+imtiaz0212@users.noreply.github.com>
2024-05-20 13:05:54 +0600

    Update laravel-helper.php

--- laravel-helper.php
+++ laravel-helper.php
@@ -8,3 +8,16 @@ RewriteCond %{HTTP_HOST} ^www\domain\.com
 RewriteRule ^(.*)$ https://domain.com$1 [R=301,L]
 
 
+
+// root command send email servicemanager
+[program:linksposting-worker]
+process_name=%(program_name)s_%(process_num)02d
+command=php /home/linksposting/public_html/artisan queue:work --queue=default --sleep=3 --tries=3
+autostart=true
+autorestart=true
+user=linksposting
+numprocs=8
+redirect_stderr=true
+stdout_logfile=/home/linksposting/public_html/storage/logs/worker.log
+
+
